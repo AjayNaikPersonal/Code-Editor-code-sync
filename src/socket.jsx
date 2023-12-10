b56f54efad31be9fe93f614836d13b9020bb8fb9
Ajay Venkataraman Naik <ajay.naik@ansrsource.com>
2023-12-10 12:39:45 +0530

    initial commit

--- src/socket.jsx
+++ src/socket.jsx
@@ -1,4 +1,16 @@
-import {io} from 'socket.io-client'
+// import {io} from 'socket.io-client'
+
+// export const initSocket = async () => {
+//     const options = {
+//         'force new connection': true,
+//         reconnectionAttempt: 'Infinity',
+//         timeout: 10000,
+//         transports: ['websocket'],
+//     };
+//     return io(import.meta.env.VITE_REACT_APP_BACKEND_URL, options);
+// };
+
+import { io } from 'socket.io-client';
 
 export const initSocket = async () => {
     const options = {
@@ -7,5 +19,12 @@ export const initSocket = async () => {
         timeout: 10000,
         transports: ['websocket'],
     };
-    return io(import.meta.env.VITE_REACT_APP_BACKEND_URL, options);
+
+    // Split the URLs based on the semicolon
+    const backendUrls = import.meta.env.VITE_REACT_APP_BACKEND_URL.split(';');
+
+    // Use the first URL in the array
+    const backendUrl = backendUrls[0];
+
+    return io(backendUrl, options);
 };
