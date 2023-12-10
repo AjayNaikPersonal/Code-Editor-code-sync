e0885c9369262262d3f165237366394c2c33a82d
Ajay Venkataraman Naik <ajay.naik@ansrsource.com>
2023-12-10 11:17:16 +0530

    initial commit  code editor:code sync

---
+++ backendActions.cjs
@@ -0,0 +1,9 @@
+// backendActions.cjs
+module.exports = {
+    JOIN: 'join',
+    JOINED: 'joined',
+    DISCONNECTED: 'disconnected',
+    CODE_CHANGE: 'code-change',
+    SYNC_CODE: 'sync-code',
+    LEAVE: 'leave',
+};
